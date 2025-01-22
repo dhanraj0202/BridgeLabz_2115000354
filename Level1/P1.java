d4c27820f842c6f32284b975f8b8e18c1a9c76d0
dhanraj0202 <dhanrajbeniwal34@gmail.com>
2025-01-22 22:49:41 +0530

    Assignment for 22/01/25 Completed

---
+++ Level1/P1.java
@@ -0,0 +1,7 @@
+public class P1{
+	public static void main(String[] args){
+		int birthYear = 2000;
+		int currentYear = 2024;
+		System.out.print("Harry's age in 2024 is " + (currentYear-birthYear));
+	}
+}
---
+++ Level1/P2.java
@@ -0,0 +1,8 @@
+public class P2{
+	public static void main(String[] args){
+		int maths = 94;
+		int physics = 95;
+		int chemistry = 96;
+		System.out.print("Sam's average mark in PCM is " + (maths+physics+chemistry)/3.0);
+	}
+}
